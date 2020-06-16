89f0094ba2d8088cf8d36bca61830dbbe481e6c8
Chege Bryan <cbbryan8@gmail.com>
2020-06-16 19:11:02 +0300

    🛡  Protect app against known web vulnerabilities

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
 var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression()); // compress all routes
+app.use(helmet()); // Protect site against known web vulnerabilities
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
