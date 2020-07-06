9172af89690c97f043d317f09f8d07b1eeac6adc
Chege Bryan <cbbryan8@gmail.com>
2020-07-06 20:07:54 +0300

    🎨 Render appropriate results page

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -120,14 +120,31 @@ exports.author_create_post = [
 
 // Display Author delete form on GET.
 exports.author_delete_get = function (req, res) {
-  async.parallel({
-    author: function (callback) {
-      Author.findById(req.params.id).exec(callback);
+  async.parallel(
+    {
+      author: function (callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books: function (callback) {
+        Book.find({ author: req.params.id }).exec(callback);
+      },
     },
-    authors_books: function (callback) {
-      Book.find({ author: req.params.id }).exec(callback);
-    },
-  });
+    function (err, results) {
+      if (err) {
+        return next(err);
+      }
+      if (results.author == null) {
+        // No results
+        res.redirect('/catalog/authors');
+      }
+      // successful, so render
+      res.render('author_delete', {
+        title: 'Delete Author',
+        author: results.author,
+        author_books: results.author_books,
+      });
+    }
+  );
 };
 
 // Handle Author delete on POST.
